2f0c18de09f93616363d0c26ba86141264a00269
Thoufeek Hussain <th7.1998@gmail.com>
2025-03-02 19:29:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def main():
             'investment_increase': 10.0
         }
 
-    st.sidebar.image("logo.png")
+    st.sidebar.image("logo.png",caption="Dream Big. We'll handle the math.")
     st.sidebar.divider()
     
     page = st.sidebar.radio("Menu", ["Profile Setup", "Financial Goals", "Recommendations","Settings"])
