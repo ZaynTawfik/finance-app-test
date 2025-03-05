6c86095019b052ced2766c6769353834f873bf83
Thoufeek Hussain <th7.1998@gmail.com>
2025-03-05 20:33:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ def main():
                 st.session_state.profile['current_portfolio'] = st.number_input("Portfolio as of today($)", min_value=0, value=3000)
 
             col3, col4 = st.columns(2)
+            st.write("Investments")
             with col3:
                 st.session_state.profile['Equity'] = st.number_input("Equity($)", min_value=0, value=3000)
             with col4:
