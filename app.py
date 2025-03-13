c06e2ef20dc8ac02ce196a8185c5185d9fc0d27e
Thoufeek Hussain <th7.1998@gmail.com>
2025-03-13 23:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def main():
                     st.session_state.settings['investment_increase'])
                 st.session_state.profile['retirement_age'] = retirement_age
                 st.session_state.profile['retirement_money'] = retirement_money
-                st.success(f"Updated Projected Retirement Age: {retirement_age} with {retirement_money}")
+                st.success(f"Updated Projected Retirement Age: {retirement_age} with {retirement_money:,.2f}")
         else:
             st.info("No goals added yet")
             
