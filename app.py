7d6e7d83c9465cb86fa0e44d7d2186476f9589d9
Thoufeek Hussain <th7.1998@gmail.com>
2025-03-17 22:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def main():
                 target_age = st.number_input("Target Age", min_value=18, max_value=100, value=28)
                 target_amount = st.number_input("Target Amount", min_value=0, value=0)
                 
-                if st.form_submit_button("Add Goal",type="tertiary):
+                if st.form_submit_button("Add Goal",type="tertiary"):
                     st.session_state.goals.append({
                         'name': goal_name,
                         'age': target_age,
