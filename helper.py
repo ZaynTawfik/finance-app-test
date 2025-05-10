c2746ff8e79d8e78000720adaef7457b231cb24c
Thoufeek Hussain <th7.1998@gmail.com>
2025-05-10 09:26:02 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,4 +2,4 @@ import os
 import streamlit as st
 
 def load_env():
-    os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+    os.environ['OPENAI_API_KEY'] = st.secrets["DEEPSEEK_API_KEY"]
